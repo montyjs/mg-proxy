d4630c2a72455538479b3024ee41900086f57b1c
Morgan Galvin <morgan@ludwigthepig.com>
2019-05-28 18:53:33 -0600

    cleaned up proxy

--- app.js
+++ app.js
@@ -1,29 +1,15 @@
 const express = require('express');
 const app = express();
-const httpProxy = require('http-proxy');
-const apiProxy = httpProxy.createProxyServer();
-const products = 'http://localhost:3001/bundle',
-      description = 'http://localhost:3002/bundle',
-      reviews = 'http://localhost:3003/bundle';
+const path = require('path');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
-app.all("/app1/*", function(req, res) {
-  console.log('redirecting to Server1');
-  apiProxy.web(req, res, {target: products});
-});
-
-app.all("/app2/*", function(req, res) {
-  console.log('redirecting to Server2');
-  apiProxy.web(req, res, {target: description});
-});
-
-app.all("/app2/*", function(req, res) {
-  console.log('redirecting to Server3');
-  apiProxy.web(req, res, {target: reviews});
-});
+app.use(bodyParser.json())
+app.use(express.static(path.join(__dirname, 'static')))
 
 app.listen(3000, err => {
   if (err) {
     return console.log(err);
   }
-  console.log('You are connected to the proxy')
+  console.log('You are connected to the proxy on port 3000')
 });
